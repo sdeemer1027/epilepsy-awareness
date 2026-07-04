d97ff7130e2c53459984fd4ff4292b2c1eb36023
sdeemer1027 <sdeemer@yahoo.com>
2026-07-04 09:32:01 -0400

    begining to end of sprint 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
+use App\Models\Role;
+
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,5 +22,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(RoleSeeder::class);
+
     }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Role::insert([
+    [
+        'id' => 1,
+        'name' => 'Administrator',
+        'slug' => 'admin',
+        'description' => 'Full system access'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Member',
+        'slug' => 'member',
+        'description' => 'Registered user'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Caregiver',
+        'slug' => 'caregiver',
+        'description' => 'Caregiver access'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Healthcare Professional',
+        'slug' => 'healthcare',
+        'description' => 'Healthcare resources'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Editor',
+        'slug' => 'editor',
+        'description' => 'Content management'
+    ]
+]);
+
+    }
+}
