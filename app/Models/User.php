c71cc8e49c2634cefa061e218e244a2f8968d716
sdeemer1027 <sdeemer@yahoo.com>
2026-07-07 12:43:32 -0400

    Add Profile relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,37 @@
+```php
 <?php
+
+declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Epilepsy Support Platform (ESP)
+ *
+ * File: User.php
+ * Purpose: Represents an authenticated user of the Epilepsy Support Platform.
+ *
+ * @package ESP
+ * @since 1.0.0-alpha
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property-read Role|null $role
+ * @property-read Profile|null $profile
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -43,8 +64,20 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function role()
+    /**
+     * Get the user's assigned role.
+     */
+    public function role(): BelongsTo
     {
         return $this->belongsTo(Role::class);
     }
+
+    /**
+     * Get the user's profile.
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
+```
