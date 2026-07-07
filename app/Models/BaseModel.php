58bc42931d5ad68fca1d267146c740e6a14d332f
sdeemer1027 <sdeemer@yahoo.com>
2026-07-07 12:21:23 -0400

    Complete Sprint 1 foundation

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Epilepsy Support Platform (ESP)
+ *
+ * File: BaseModel.php
+ * Purpose: Base Eloquent model for all ESP application models.
+ *
+ * @package ESP
+ * @since 1.0.0-alpha
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * Child models should override this property.
+     *
+     * @var array<int,string>
+     */
+    protected $fillable = [];
+
+    /**
+     * Child models should override this property.
+     *
+     * @var array<string,string>
+     */
+    protected $casts = [];
+}
